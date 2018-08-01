637adcd3e13ea43c4983439f3acd0e5e95c06cae
Jakob Willforss <jakob.willforss@hotmail.com>
2018-08-01 09:28:21 +0200

    Basic return-based iteration with hints in place!

--- app.R
+++ app.R
@@ -1,19 +1,19 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 # library(shinyWidgets)
 
 source("functions.R")
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
+    
+    # Press return to trig check
+    tags$script(HTML("$(function(){
+      $(document).keyup(function(e) {
+      if (e.which == 13) {
+        $('#iterate').click()
+      }
+      });
+      })")),
+    
     
     # Application title
     titlePanel("Learn chinese characters - with RShiny"),
@@ -21,73 +21,51 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            # sliderInput("bins",
-            #             "Number of bins:",
-            #             min = 1,
-            #             max = 50,
-            #             value = 30),
-            # 
-            # sliderInput("bins",
-            #             "Number of bins:",
-            #             min = 1,
-            #             max = 50,
-            #             value = 30),
-            
-            # actionButton("iterate", "Click me"),
-            # actionButton("iterate2", "Click me 2"),
-            # actionButton("iterate3", "Click me 3"),
-            # uiOutput("my_button"),
-            
+            selectInput(
+                inputId = 'hint_level', 
+                label = 'Hint level', 
+                choices = c("None", "Tone", "Pinying", "English", "All")
+            ),
             textInput("pinying", "Enter pinying"),
             textInput("english", "Enter english"),
-            actionButton("iterate", "Enter")
-
-            # checkboxGroupButtons(
-            #     inputId = "somevalue", label = "Make a choice :", 
-            #     choices = c("Choice A", "Choice B", " Choice C", "Choice D"), 
-            #     justified = TRUE, status = "primary",
-            #     checkIcon = list(yes = icon("ok", lib = "glyphicon"), no = icon("remove", lib = "glyphicon"))
-            # )
-            
-                        
-            # dropdownButton(
-            #     selectInput(inputId = 'xcol', label = 'X Variable', choices = c("A", "B"))
-            # )
+            actionButton("iterate", "Enter"),
+            htmlOutput("result")
         ),
         
-        # Show a plot of the generated distribution
         mainPanel(
             htmlOutput("char_display"),
             htmlOutput("text"),
             htmlOutput("statistics"),
-            htmlOutput("Hint:"),
+            htmlOutput("hint"),
             plotOutput("distPlot")
         )
     )
+    
+    
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
     
     cur_ind <- 1
-    
     correct <- 0
     correct_pinying <- 0
     correct_english <- 0
     total <- 0
     
+    result_display <- TRUE
+    
     dict <- list()
-    dict[[1]] <- c("一", "yi1", "one")
-    dict[[2]] <- c("丨", "gun3", "line")
-    dict[[3]] <- c("丶", "zhu3", "dot")
-    dict[[4]] <- c("丿	乀", "fu2", "slash")
-    dict[[5]] <- c("乙 乚", "yin3", "second")
-    dict[[6]] <- c("亅", "jue2", "hook")
-    dict[[7]] <- c("二", "er4", "two")
-    dict[[8]] <- c("亠", "tou2", "lid")
-    dict[[9]] <- c("人 亻", "ren2", "man")
-    dict[[10]] <- c("儿", "er2", "legs")
-
+    dict[[1]] <- c("一", "yi1", "one", 1)
+    dict[[2]] <- c("丨", "gun3", "line", 3)
+    dict[[3]] <- c("丶", "zhu3", "dot", 3)
+    dict[[4]] <- c("丿	乀", "fu2", "slash", 2)
+    dict[[5]] <- c("乙 乚", "yin3", "second", 3)
+    dict[[6]] <- c("亅", "jue2", "hook", 2)
+    dict[[7]] <- c("二", "er4", "two", 4)
+    dict[[8]] <- c("亠", "tou2", "lid", 2)
+    dict[[9]] <- c("人 亻", "ren2", "man", 2)
+    dict[[10]] <- c("儿", "er2", "legs", 2)
+    
     output$my_button <- renderUI({
         actionButton("iterate", label=paste("text -", cur_ind))
     })
@@ -96,38 +74,64 @@ server <- function(input, output) {
         paste0("<div style='font-size:30px'>", get_char_string(dict), "</div>")
     })
     
-    # output$distPlot <- renderPlot({
-    #     x    <- faithful[, 2] 
-    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    # })
-    
+    output$hint <- renderText({
+        paste0("<div style='font-size:30px'>", 
+               get_hint_text(dict[[cur_ind]], input[["hint_level"]]),
+               "</div>")
+    })
+
     output$text <- renderText({paste0('<div style="font-size:200px;">', dict[[cur_ind]][1], '</div>')})
 
     observeEvent(input$iterate, {
         
-        print(get_char_string(dict))
+        # print(get_char_string(dict))
+        print("Iterate")
+        print(result_display)
+        # print(check_correct(dict, cur_ind, input[["pinying"]], input[["english"]]))
 
-        print(check_correct(dict, cur_ind, input[["pinying"]], input[["english"]]))
-                
-        total <<- total + 1
-        cur_ind <<- cur_ind + 1
-        cur_ind <- (cur_ind - 1) %% length(dict) + 1
-        print(paste("New index: ", cur_ind))
-        
-        output$text <- renderText({paste0('<div style="font-size:200px;">', dict[[cur_ind]][1], '</div>')})
+        if (result_display) {
+            result_display <<- FALSE
+            output$result <- renderText(get_result_string(dict, cur_ind))
+        }
+        else {
+            # Iterate update
+            result_display <<- TRUE
+            output$result <- renderText("")
+            total <<- total + 1
+            cur_ind <<- cur_ind + 1
+            cur_ind <- (cur_ind - 1) %% length(dict) + 1
+            print(paste("New index: ", cur_ind))
+            
+            # Clear previous
+            updateTextInput(session, "english", label = NULL, value = "")
+            updateTextInput(session, "pinying", label = NULL, value = "")
+            
+            # input[["pinying"]] <<- ""
+            # input[["english"]] <<- ""
+            
+            # Update character
+            output$text <- renderText({paste0('<div style="font-size:200px;">', dict[[cur_ind]][1], '</div>')})
+            
+            output$my_button <- renderUI({
+                actionButton("iterate", label=paste("text -", cur_ind))
+            })
 
-        output$my_button <- renderUI({
-            actionButton("iterate", label=paste("text -", cur_ind))
-        })
-        
-        output$statistics <- renderText({
-            paste(
-                'Correct: ', correct, 
-                '<br>Correct Pinying: ', correct_pinying, 
-                '<br>Correct English: ', correct_english,
-                '<br>Total: ', total)
-        })
+            # Hint
+            output$hint <- renderText({
+                paste0("<div style='font-size:30px'>", 
+                       get_hint_text(dict[[cur_ind]], input[["hint_level"]]),
+                       "</div>")
+            })
+            
+            # Statistics
+            output$statistics <- renderText({
+                paste(
+                    'Correct: ', correct, 
+                    '<br>Correct Pinying: ', correct_pinying, 
+                    '<br>Correct English: ', correct_english,
+                    '<br>Total: ', total)
+            })
+        }
     })
 }
 
--- functions.R
+++ functions.R
@@ -2,6 +2,42 @@ test_print <- function() {
     print("Executing a test print")
 }
 
+get_hint_text <- function(char_entry, hint_level) {
+    
+    if (hint_level == "None") {
+        hint_string <- ""
+    }
+    else if (hint_level == "Tone") {
+        
+        tones <- c("má", "mā", "mǎ", "mà","ma")
+        tone_level <- as.numeric(char_entry[4])
+        print(paste("Tone level:", tone_level, "tone:", tones[tone_level]))
+        hint_string <- paste("Tone:", tones[tone_level])
+    }
+    else if (hint_level == "Pinying") {
+        hint_string <- paste("Pinying:", char_entry[2])
+    }
+    else if (hint_level == "English") {
+        hint_string <- paste("English:", char_entry[3])
+    }
+    else if (hint_level == "All") {
+        hint_string <- paste0(
+            "English: ", char_entry[3], ", ", 
+            "Pinying: ", char_entry[2])
+    }
+    else {
+        stop(paste("Unknown hint level: ", hint_level))
+    }
+    
+    hint_string
+}
+
+get_result_string <- function(char_list, index) {
+    expected_pinying <- char_list[[index]][2]
+    expected_english <- char_list[[index]][3]
+    paste("Correct:", expected_english, expected_pinying)
+}
+
 get_char_string <- function(char_list) {
     
     char_vect <- unlist(lapply(
